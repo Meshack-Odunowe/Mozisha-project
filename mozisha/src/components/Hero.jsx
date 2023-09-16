e304e7dd3327120669ecf0e8d66a1a732b1f56c5
Meshack <meshack.odunowe@decagon.dev>
2023-09-16 12:30:39 +0100

    Initial commit

---
+++ mozisha/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CountUp from "react-countup";
+const Hero = () => {
+  return (
+    <div data-aos="fade-right" data-aos-duration="1500">
+      Hero
+    </div>
+  );
+};
+
+export default Hero;
